074185482951e83e05767edbb668ca620dd9bd61
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-08-04 13:35:51 -0600

    feat: refactored logic out of serializer

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,7 +1,8 @@
 class WeatherFacade
   def current_weather(params)
     location = MapquestService.new.coordinates(params)
-    report = WeatherService.new.weather_report(location.coordinates)
+    report_hash = WeatherService.new.weather_report(location.coordinates)
+    report = WeatherReport.new(report_hash)
     WeatherReportSerializer.new(report).serialize_json
   end
 end
--- app/poros/weather_report.rb
+++ app/poros/weather_report.rb
@@ -4,16 +4,16 @@ class WeatherReport
               :hourly_weather_array
 
   def initialize(weather_data)
-    @last_updated = @weather_data[:current][:last_updated]
-    @temperature = @weather_data[:current][:temp_f]
-    @feels_like = @weather_data[:current][:feelslike_f]
-    @humidity = @weather_data[:current][:humidity]
-    @uvi = @weather_data[:current][:uv]
-    @visibility = @weather_data[:current][:vis_miles]
-    @condition = @weather_data[:current][:condition][:text]
-    @icon = @weather_data[:current][:condition][:icon]
-    @daily_weather_array = set_daily_weather_array(@weather_data[:forecast][:forecastday])
-    @hourly_weather_array = set_hourly_weather_array(@weather_data[:forecast][:forecastday].first[:hour])
+    @last_updated = weather_data[:current][:last_updated]
+    @temperature = weather_data[:current][:temp_f]
+    @feels_like = weather_data[:current][:feelslike_f]
+    @humidity = weather_data[:current][:humidity]
+    @uvi = weather_data[:current][:uv]
+    @visibility = weather_data[:current][:vis_miles]
+    @condition = weather_data[:current][:condition][:text]
+    @icon = weather_data[:current][:condition][:icon]
+    @daily_weather_array = set_daily_weather_array(weather_data[:forecast][:forecastday])
+    @hourly_weather_array = set_hourly_weather_array(weather_data[:forecast][:forecastday].first[:hour])
   end
 
   def set_daily_weather_array(days)
