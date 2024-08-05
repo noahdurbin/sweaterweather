1138f17874a29a73d414baca5f9a09ebff586a10
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-08-05 11:55:38 -0600

    feat: book list spec

--- spec/poros/book_list_spec.rb
+++ spec/poros/book_list_spec.rb
@@ -1,26 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe 'book list' do
-  describe 'initialize' do
-    it 'can create a new book list' do
-      book_hash = {
-        numFound: 39,
-        docs: [
-          {
-            isbn: 123456,
-            title: "title",
-            publisher: "book publisher"
-          },
-          {
-            isbn: 12456,
-            title: "book",
-            publisher: "publisher of books"
-          }
-        ]
-      }
-      book_list = BookList.new(book_hash)
+  before(:all) do
+    book_hash = {
+      numFound: 39,
+      docs: [
+        {
+          isbn: 123456,
+          title: "title",
+          publisher: "book publisher"
+        },
+        {
+          isbn: 12456,
+          title: "book",
+          publisher: "publisher of books"
+        }
+      ]
+    }
+    @book_list = BookList.new(book_hash)
+  end
 
-      expect(book_list).to be_a BookList
+  describe 'initialize' do
+    it 'can create a book list' do
+      expect(@book_list).to be_a BookList
+    end
+  end
+
+  describe 'methods' do
+    it 'can read attributes' do
+      expect(@book_list.results_quantity).to eq 39
     end
   end
 end
