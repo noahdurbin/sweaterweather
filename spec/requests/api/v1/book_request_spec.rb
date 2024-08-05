fa21952f09b4af2d27a64a750ac7a02c2a2c6da4
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-08-05 09:29:56 -0600

    test: further testing for the book api response

--- spec/requests/api/v1/book_request_spec.rb
+++ spec/requests/api/v1/book_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'retrieves weather and books for a location' do
-  it 'can take a city as an input and give back related books and the current weather' do
+  it 'can take a city as an input and give back related books and the current weather', :vcr do
     get "/api/v1/book-search?location=dever,co&quantity=5"
 
     expect(response).to be_successful
@@ -12,6 +12,19 @@ RSpec.describe 'retrieves weather and books for a location' do
 
     expect(report[:data]).to have_key :id
     expect(report[:data][:id]).to eq nil
-    expect()
+    expect(report[:data][:type]).to eq "books"
+
+    expect(report[:data]).to have_key :attributes
+    expect(report[:data][:attributes].count).to eq 4
+    expect(report[:data][:attributes][:destination]).to eq "denver,co"
+    expect(report[:data][:attributes][:forecast].count).to eq 2
+    expect(report[:data][:attributes][:forecast]).to have_key :summary
+    expect(report[:data][:attributes][:forecast]).to have_key :temperature
+
+    expect(report[:data][:attributes]).to have_key :total_books_found
+    expect(report[:data][:attributes]).to have_key :books
+
+    expect(report[:data][:attributes][:books]).to be_a Hash
+    expect(report[:data][:attributes][:books].count).to eq 5
   end
 end
